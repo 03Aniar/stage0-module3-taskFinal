4310d478379aae4aabdebb364ab23c18619c667b
aniar <aniar.kaldybaiuly@nu.edu.kz>
2023-03-21 08:06:28 +0600

    v1

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,15 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int f,s,t,fo;
+        f = number % 10;
+        number /= 10;
+        s = number % 10;
+        number /= 10;
+        t = number % 10;
+        number /= 10;
+        fo = number;
+        System.out.println(f + fo + t + s);
+
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int f,s,t;
+        f = number % 10;
+        number /= 10;
+        s = number % 10;
+        number /= 10;
+        t = number;
+        System.out.println(f * 100 + s * 10 + t);
     }
 }
